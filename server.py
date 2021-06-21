63c34b2958eff88e9e09311c2f2ac49bd7bba962
Szymon Piskorz <spiskorz@cisco.com>
2021-06-21 23:51:36 +0200

    another one

--- server.py
+++ server.py
@@ -1,3 +1,19 @@
 import os
 print(os.listdir())
 print(os.path)
+import sys
+import socket
+
+class Server:
+    def __init__(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind(('', 2137))
+        except socket.error:
+            print('Failed')
+            sys.exit()
+
+        self.socket.listen(5)
+        print('Listening')
+
+x=Server()
